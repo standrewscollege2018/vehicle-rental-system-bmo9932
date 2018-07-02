b6ec00b1e8fe64a0ed1b5d3fe9c9301cd5ecd1cf
bmo9932 <40441859+bmo9932@users.noreply.github.com>
2018-07-03 09:56:34 +1200

    Added returning function

--- vms.py
+++ vms.py
@@ -1,10 +1,11 @@
 class Vehicle:
-    def __init__(self, name, seats, licence, avaiable):
+    def __init__(self, name, seats, licence, avaiable, renter, cost):
         self._name = name
         self._seats = seats
         self._licence = licence
         self._available = avaiable
-        self._renter = ''
+        self._renter = renter
+        self._cost = cost
         vehicles.append(self)
 
     def _display_vehicles(self):
@@ -12,20 +13,34 @@ class Vehicle:
         print('Name : {}'.format(self._name))
         print('Seats : {}'.format(self._seats))
         print('Licence : {}'.format(self._licence))
+        print('${} per day'.format(self._cost))
+        print('----------------------')
+
+    def _display_vehicles_rented(self):
+        print('----------------------')
+        print('Name : {}'.format(self._name))
+        print('Seats : {}'.format(self._seats))
+        print('Licence : {}'.format(self._licence))
+        print('${} per day'.format(self._cost))
+        print('Rented by : {}'.format(self._renter))
         print('----------------------')
 
     def _rent_vehicle(self):
         self._renter = renter_name
         self._available = False
 
+    def _return_vehicle(self):
+        self._renter = ''
+        self._available = True
+
 vehicles = []
 
-Vehicle('Ford Falcon', 5 , 'GTF724', True)
-Vehicle('Dodge Challenger', 4 , 'BAT528', True)
-Vehicle('Subaru WRX', 5 , 'GOX654', False)
-Vehicle('Toyota Supra', 4 , 'MHQ287', True)
-Vehicle('Ford Ranger', 5 , 'RAQ812', True)
-Vehicle('Honda Odyssey', 7 , 'NPQ114', False)    
+Vehicle('Ford Falcon', 5 , 'GTF724', True, '', 80)
+Vehicle('Dodge Challenger', 4 , 'BAT528', True, '', 110)
+Vehicle('Subaru WRX', 5 , 'GOX654', False, 'Betty', 100)
+Vehicle('Toyota Supra', 4 , 'MHQ287', True, '', 95)
+Vehicle('Ford Ranger', 5 , 'RAQ812', True, '', 90)
+Vehicle('Honda Odyssey', 7 , 'NPQ114', False, 'John', 65)    
 
 
 print("Welcome to BRAD's vehicle rental system")
@@ -62,10 +77,23 @@ while True:
                 print('No Vehicles found')
             else:
                 print("Thanks For renting with BRAD's vehicle rental system")
-                
     elif x == 2:
-        return_vehicle()
-
-
+        counter = 0
+        for v in vehicles:
+            if v._available == False:
+                counter += 1
+                v._display_vehicles_rented()
+        if counter == 0:
+            print('No Vehicles Rented ')
+        else:
+            chosen_vehicle = input('Please enter the licence of the vehicle you wish to return: ')
+            counter = 0
+            for b in vehicles:
+                if chosen_vehicle in b._licence and b._available == False:
+                    b._return_vehicle()
+                    counter += 1
+                    break
+            if counter == 0:
+                print('No Vehicles found')
 
 
