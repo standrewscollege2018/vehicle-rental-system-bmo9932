fc94c7537d0488032f8d9060fae426474a375007
bmo9932 <40441859+bmo9932@users.noreply.github.com>
2018-07-04 13:56:48 +1200

    Added commenting

--- vms.py
+++ vms.py
@@ -1,6 +1,5 @@
-# comment your code!!!
-
 class Vehicle:
+    # Function to get the objects listed below and extract the variables within them add adding them to the list vehicles
     def __init__(self, name, seats, licence, avaiable, renter, cost):
         self._name = name
         self._seats = seats
@@ -10,6 +9,8 @@ class Vehicle:
         self._cost = cost
         vehicles.append(self)
 
+    # Function to display the vehicles in the rent option
+    # Used to format this infomation to make it look cleaner
     def _display_vehicles(self):
         print('----------------------')
         print('Name : {}'.format(self._name))
@@ -18,6 +19,8 @@ class Vehicle:
         print('${} per day'.format(self._cost))
         print('----------------------')
 
+    # Function to display the vehicles in the return option
+    # Same as above except showing who is renting the car 
     def _display_vehicles_rented(self):
         print('----------------------')
         print('Name : {}'.format(self._name))
@@ -27,16 +30,20 @@ class Vehicle:
         print('Rented by : {}'.format(self._renter))
         print('----------------------')
 
+    # Used to change the availability variable to false and set the renter name.
     def _rent_vehicle(self):
         self._renter = renter_name
         self._available = False
 
+    # Used to set the availability to True and set the renter name to blank 
     def _return_vehicle(self):
         self._renter = ''
         self._available = True
 
+# Setting the list to empty that stores the objects.
 vehicles = []
 
+# Setting the objects that will be displayed in the list 
 Vehicle('Ford Falcon', 5 , 'GTF724', True, '', 80)
 Vehicle('Dodge Challenger', 4 , 'BAT528', True, '', 110)
 Vehicle('Subaru WRX', 5 , 'GOX654', False, 'Betty', 100)
@@ -45,11 +52,13 @@ Vehicle('Ford Ranger', 5 , 'RAQ812', True, '', 90)
 Vehicle('Honda Odyssey', 7 , 'NPQ114', False, 'John', 65)    
 
 
+# Welcome message and while true loop so that you can do multiple things in one session
 print("Welcome to BRAD's vehicle rental system")
 while True:
     print("Input 1 to rent a vehicle.")
     print("Input 2 to return a vehicle.")
     print("Input 3 to add a new vehicle.")
+    # Error catching for insuring the input is 1 , 2 or 3
     while True:
         try:
             x = int(input(':'))
@@ -59,6 +68,7 @@ while True:
                 print('Please enter either 1 to rent a vehicle or 2 to return')
         except:
             print('Please enter an integer. \n Either 1 to rent a vehicle or 2 to return')
+    # Code for displaying vehicles and renting a vehicle
     if x == 1:
         counter = 0
         for v in vehicles:
@@ -80,6 +90,7 @@ while True:
                 print('No Vehicles found')
             else:
                 print("Thanks For renting with BRAD's vehicle rental system")
+    # Code for returning a vehicle
     elif x == 2:
         counter = 0
         for v in vehicles:
@@ -98,6 +109,7 @@ while True:
                     break
             if counter == 0:
                 print('No Vehicles found')
+    # Code for adding a new car
     elif x == 3:
         name = input('Name of car : ')
         seats = input('Number of seats :')
